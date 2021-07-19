55cc32537c5cffc5c59080df97f878447f5d1069
Preetham B <preethamb97@gmail.com>
2021-07-20 00:38:18 +0530

    update

--- projectsource/main.js
+++ projectsource/main.js
@@ -4,18 +4,38 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const cluster = require('cluster');
 const { cpus } = require('os');
+const { lookup } = require('geoip-lite');
 
+const router = express.Router();
 const app = express();
 const numOfCPUs = cpus().length;
 
 app.use(helmet());
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  console.table([{ success: true, processid: process.pid }])
+router.get('*', (req, res, next) => {
+  console.table([{
+    user_ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+    process_id: process.pid
+  }]);
+  next();
+});
+router.get('/', (req, res) => {
   res.send({ success: true, processid: process.pid });
 });
 
+router.get('/user_ip', (req, res) => {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  res.send({ your_ip_address: ip });
+});
+
+router.get('/ip_location', (req, res) => {
+  const ipAddress = JSON.parse(JSON.stringify(req.query.ip_lookup));
+  console.log(ipAddress)
+  res.send({ ip_address: ipAddress, ip_location: lookup(ipAddress) });
+});
+
+app.use('/', router);
 if (cluster.isMaster) {
   for (let i = 0; i < numOfCPUs; i++) {
     cluster.fork();
@@ -26,8 +46,8 @@ if (cluster.isMaster) {
     cluster.fork();
   });
 } else {
-  app.listen(API_PORT, () => {
-    console.log(`Server started on pid ${process.pid} ay port ${API_PORT}`);
+  app.listen(API_PORT, '127.0.0.1', () => {
+    console.log(`Server started on pid ${process.pid} at port ${API_PORT}`);
   });
 }
 
