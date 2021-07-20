97af2c7def3e3200c7ce2bcc885e915b37f01d38
Preetham B <preethamb97@gmail.com>
2021-07-20 23:59:03 +0530

    opencv intergrated

--- projectsource/main.js
+++ projectsource/main.js
@@ -1,17 +1,36 @@
-const { API_PORT } = require('./settings/settings');
+const { API_PORT, STREAM_FRAMES, WEB_CAM_STREAMING_STATUS } = require('./settings/settings');
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const cluster = require('cluster');
 const { cpus } = require('os');
 const { lookup } = require('geoip-lite');
-
+const path = require('path');
 const router = express.Router();
 const app = express();
-const numOfCPUs = cpus().length;
+const server = require('http').Server(app);
+const io = require('socket.io')(server, { allowEIO3: true });
+// var io = require('socket.io')(server, {'transports': ['websocket', 'polling']});
+const opencv = require('opencv4nodejs');
 
-app.use(helmet());
-app.use(morgan('dev'));
+const numOfCPUs = cpus().length;
+var isVideoStreamingLive = false;
+
+if (WEB_CAM_STREAMING_STATUS) {
+  const videoCapture = new opencv.VideoCapture(0);
+  videoCapture.set(opencv.CAP_PROP_FRAME_WIDTH, 300);
+  videoCapture.set(opencv.CAP_PROP_FRAME_HEIGHT, 300);
+  setInterval(() => {
+    const frame = videoCapture.read();
+    if (frame.step > 0) {
+      isVideoStreamingLive = true;
+      const image = opencv.imencode('.jpg', frame).toString('base64');
+      io.emit('image', image);
+    } else {
+      isVideoStreamingLive = false;
+    }
+  }, 1000 / STREAM_FRAMES);
+}
 
 router.get('*', (req, res, next) => {
   console.table([{
@@ -35,7 +54,21 @@ router.get('/ip_location', (req, res) => {
   res.send({ ip_address: ipAddress, ip_location: lookup(ipAddress) });
 });
 
+app.get('/webcam', (req, res) => {
+  if (!WEB_CAM_STREAMING_STATUS || !isVideoStreamingLive) {
+    res.send({ webcam_status: 'off' });
+  }
+  res.sendFile(path.join(__dirname, '/webpages/webcam.html'));
+});
+
+app.use('*', (req, res) => {
+  res.send("Page not found");
+})
+
+app.use(helmet());
+app.use(morgan('dev'));
 app.use('/', router);
+
 if (cluster.isMaster) {
   for (let i = 0; i < numOfCPUs; i++) {
     cluster.fork();
@@ -46,7 +79,7 @@ if (cluster.isMaster) {
     cluster.fork();
   });
 } else {
-  app.listen(API_PORT, '127.0.0.1', () => {
+  server.listen(API_PORT, '127.0.0.1', () => {
     console.log(`Server started on pid ${process.pid} at port ${API_PORT}`);
   });
 }
--- projectsource/settings/settings.js
+++ projectsource/settings/settings.js
@@ -1,1 +1,3 @@
 exports.API_PORT = 4000;
+exports.STREAM_FRAMES = 10;
+exports.WEB_CAM_STREAMING_STATUS = 1;
